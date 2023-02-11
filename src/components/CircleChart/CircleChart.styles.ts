5c549f991472a274298fcaaea8901fee8fbbff30
Clarissa Xavier <clarissaxr@gmail.com>
2023-02-11 13:26:03 -0300

    setup e logica do componente

--- src/components/CircleChart/CircleChart.styles.ts
+++ src/components/CircleChart/CircleChart.styles.ts
@@ -1,6 +1,50 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
 
-  
-`
+export const SvgWrapper = styled.div`
+  position: relative;
+  &:not(:last-child) {
+    margin-bottom: 16px;
+  }
+`;
+
+export const Svg = styled.svg`
+  transform: rotate(90deg);
+`;
+
+export const Circle = styled.circle`
+  transition: stroke-dashoffset 850ms ease;
+`;
+
+export const CircleBG = styled.circle.attrs({
+  fill: "#fff",
+})``;
+
+export const Percentage = styled.span<{theme: 'default' | 'primary'}> `
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #274060;
+  font-weight: 900;
+  font-size: 1.2em;
+  color: ${props => props.theme === 'default' ? '#09f' :  '#274060'};
+`;
+
+export const Caption = styled.span<{theme: 'default' | 'primary'}>`
+  font-size: 1em;
+  font-weight: 400;
+  text-transform: lowercase;
+  color: ${props => props.theme === 'primary' ? '#09f' :  '#274060'};
+`;
--- src/components/CircleChart/CircleChart.tsx
+++ src/components/CircleChart/CircleChart.tsx
@@ -5,15 +5,19 @@ export interface CircleChartProps {
   size: number;
   progress: number;
   caption?: string;
-  theme?: 'default' | 'primary';
+  theme?: "default" | "primary";
   strokeWidth?: number;
 }
 
-export function CircleChart({ size, progress, caption, theme, strokeWidth }: CircleChartProps) {
+export function CircleChart({
+  size,
+  progress,
+  caption,
+  theme,
+  strokeWidth,
+}: CircleChartProps) {
   //função que recupera a cor do chart com base no tema
-  const getThemeColor = () => {
-    theme === 'primary' ? '#09f' : '274060'
-  }
+  const getThemeColor = () => (theme === "primary" ? "#09f" : "#274060");
 
   //setup (configurações de cor, borda, etc)
   const THEME = getThemeColor();
@@ -31,11 +35,28 @@ export function CircleChart({ size, progress, caption, theme, strokeWidth }: Cir
   //observador para animar o offset
   useEffect(() => {
     const progressOffset = ((100 - progress) / 100) * CIRCUMFERENCE;
-    setOffset(progressOffset)
-  }, [setOffset, CIRCUMFERENCE, progress, offset])
-
+    setOffset(progressOffset);
+  }, [setOffset, CIRCUMFERENCE, progress, offset]);
 
   return (
-    <CC.Wrapper>CircleChart</CC.Wrapper>
-  )
+    <CC.Wrapper>
+      <CC.SvgWrapper style={{ width: size, height: size }}>
+        <CC.Svg width={size} height={size}>
+          <CC.CircleBG cy={CENTER} cx={CENTER} r={RADIUS} />
+          <CC.Circle
+            fill="none"
+            cy={CENTER}
+            cx={CENTER}
+            r={RADIUS}
+            stroke={STROKE_COLOR}
+            strokeWidth={STROKE_WIDTH}
+            strokeDasharray={CIRCUMFERENCE}
+            strokeDashoffset={offset}
+          />
+        </CC.Svg>
+        <CC.Percentage>{progress}%</CC.Percentage>
+      </CC.SvgWrapper>
+      {caption && <CC.Caption>{caption}</CC.Caption>}
+    </CC.Wrapper>
+  );
 }
