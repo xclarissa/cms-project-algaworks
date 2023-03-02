412420f41380800ed891a97e26909c984b33fdf6
xclarissa <clarissaxr@gmail.com>
2023-03-02 12:30:22 -0300

    Enviando o arquivo pro storage

--- src/sdk/services/File.service.ts
+++ src/sdk/services/File.service.ts
@@ -2,10 +2,19 @@ import { File } from "../@types";
 import Service from "../Service";
 
 class FileService extends Service {
+  // recupera url assinada 
   static getSignedUrl(fileInfo: File.UploadRequestInput) {
     return this.Http.post<File.UploadRequest>("/upload-requests", fileInfo)
       .then(this.getData)
       .then((res) => res.uploadSignedUrl);
+  }
+
+  // envia arq para url assinada
+  static uploadFileToSignedUrl(signedUrl: string, file: File) {
+    return this.Http.put<{}>(signedUrl, file, {
+      headers: { "Content-Type": file.type }
+    })
+      .then(this.getData)
   }
 }
 
